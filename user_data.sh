66e80927fd8d2b34cc601e68b285d9453b02fb1f
nagadurga8 <125259990+nagadurga8@users.noreply.github.com>
2023-04-01 22:13:21 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum remove awscli -y
+apt-get remove awscli -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install

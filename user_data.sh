073f82d57804169ccf9c034df0eedfd713552117
nagadurga8 <125259990+nagadurga8@users.noreply.github.com>
2023-04-05 14:40:51 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -8,6 +8,7 @@ sudo ./aws/install
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.25.7/2023-03-17/bin/linux/amd64/kubectl
 chmod +x ./kubectl
+mv kubectl /usr/local/bin
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
